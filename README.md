712bc91f83de2426694b9d1b6f8d624b7a844896
ShellBin <maoxiaobing@live.com>
2020-01-28 23:30:00 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # NS_Showcase
- Nintendo Switch 游戏陈列柜
+ Nintendo Switch 游戏陈列柜  
+ 
+## About
+ 实现一个优雅好看的，可以展示你玩过哪些 NS 游戏的陈列柜，以弥补Nintendo Online在这方面的功能不足  
+ 
+## TODO (基础)
+ - [ ] 前台展现
+   - [ ] HTML
+   - [ ] CSS
+   - [ ] JavaScript
+   - [ ] PHP
+ - [ ] 后台管理
+   - [ ] HTML
+   - [ ] CSS
+   - [ ] JavaScript
+   - [ ] PHP
+ - [ ] SQL
+ 
+## TODO（功能）
+（）
+ 
+## 备注 
+ 便于托管在主机壳，项目使用 PHP 完成  
+ 先随便做着，一言不合应该就咕了
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nintendo Switch ShellBin's Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/uikit.min.css" />
+    <link rel="stylesheet" href="/css/index.css" />
+    <script src="/js/uikit.min.js"></script>
+    <script src="/js/uikit-icons.min.js"></script>
+</head>
+<body>
+<div uk-sticky="media: 960" class="header">
+    <img src="/img/animal%20crossing.png" style="height: 20px">
+    &nbsp;
+    <a href="https://shellbin.me">ShellBin's Page</a>
+</div>
+
+<div class="page-view">
+    <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin profile" uk-grid >
+        <div class="uk-card-media-left uk-cover-container">
+            <img src="/img/upload/avatar/avatar.jpg" alt="" uk-cover>
+            <canvas width="600" height="400"></canvas>
+        </div>
+        <div>
+            <div class="uk-card-body">
+                <h3 class="uk-card-title">ShellBin</h3>
+                <p>好友编号</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+</body>
+</html>
